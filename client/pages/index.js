2e7a404deb6533a59cd6dcf002bdd6cdfd82e429
Long Do <longdo@dobtech.vn>
2021-10-29 00:17:24 +0700

    add tickets table

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,33 @@
-const LandingPage = ({ currentUser }) => {
-  return <h1>{currentUser ? 'You are signed in' : 'You are NOT signed in'}</h1>;
+const LandingPage = ({ tickets }) => {
+  const ticketList = tickets.map((ticket) => {
+    return (
+      <tr key={ticket.id}>
+        <td>{ticket.title}</td>
+        <td>{ticket.price}</td>
+      </tr>
+    );
+  });
+
+  return (
+    <div>
+      <h1>Tickets</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>{ticketList}</tbody>
+      </table>
+    </div>
+  );
+};
+
+LandingPage.getInitialProps = async (context, client) => {
+  const { data } = await client.get('/api/tickets');
+
+  return { tickets: data };
 };
 
 export default LandingPage;
