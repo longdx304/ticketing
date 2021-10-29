2ccb82c63fb6671863d4f833073e33d5ed245ef4
Long Do <longdo@dobtech.vn>
2021-10-29 23:31:28 +0700

    trigger first time CI/CD process

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 const buildClient = ({ req }) => {
   if (typeof window === 'undefined') {
     return axios.create({
+      // use http://ingress-nginx-controller.ingress-nginx.svc.cluster.local for developing on local machine
       baseURL: 'http://www.tocky.vn',
       headers: req.headers,
     });
